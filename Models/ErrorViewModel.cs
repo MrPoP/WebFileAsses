5ba53e10e800887e4ef90743c584e43e75e37744
Mahmoud PoP <modyali_Fox@yahoo.com>
2024-01-10 16:06:27 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebFileAsses.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ Models/Priority.cs
@@ -0,0 +1,9 @@
+﻿namespace WebFileAsses.Models
+{
+	public class Priority
+	{
+		public byte ID { get; set; }
+		public string? Name { get; set; }
+		public string? FilePath { get; set; }
+	}
+}
